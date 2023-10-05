fd72828690435f2d34be058d35af4e0f32437d3b
Preston Roesslet <proesslet@gmail.com>
2023-10-04 23:07:07 -0500

    Update router.js

--- Gadget-Grammar/src/router.js
+++ Gadget-Grammar/src/router.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import Home from "./views/Home.vue";
 
 const router = createRouter({
-  history: createWebHistory('/actionstesting/),
+  history: createWebHistory('/actionstesting/'),
   routes: [
     {
       path: "/",

ac69ac36bd44c3902f0f29b45d8ca6a165c1b0b7
Preston Roesslet <proesslet@gmail.com>
2023-10-04 22:50:55 -0500

    init

---
+++ Gadget-Grammar/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container-fluid">
+    <RouterView />
+  </div>
+</template>
+
+<script>
+import { RouterView } from "vue-router";
+
+export default {
+  name: "App",
+  components: {},
+};
+</script>
+
+<style scoped></style>
